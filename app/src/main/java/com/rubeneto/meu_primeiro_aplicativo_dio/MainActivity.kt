c22341311fec0cb40a808812cbf2be28d27e1f76
rubendev7l <rubencandidog@gmail.com>
2023-09-13 19:05:15 -0300

    Commit desafio alunos Dio do curso Entendendo a Estrutura e Depurando Apps Android

---
+++ app/src/main/java/com/rubeneto/meu_primeiro_aplicativo_dio/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rubeneto.meu_primeiro_aplicativo_dio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //O desafio se configura em criar um  vaalor dentro de uma string.xml
+        //E trocar o texto do xml usando outras linguagens (Inglês, Espanhol, etc.)
+    }
+}
